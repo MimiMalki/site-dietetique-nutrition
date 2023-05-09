20bfa4a65779760245f2f15f86c204a3030116a5
Meryem Malki <meriamemalki96@gmail.com>
2023-05-09 12:36:40 +0200

    Add entity Allergie and relation MTM with entity User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -35,6 +37,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255)]
     private ?string $numTelephone = null;
+
+    #[ORM\ManyToMany(targetEntity: Allergie::class, mappedBy: 'user')]
+    private Collection $allergies;
+
+    public function __construct()
+    {
+        $this->allergies = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -138,6 +148,33 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setNumTelephone(string $numTelephone): self
     {
         $this->numTelephone = $numTelephone;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Allergie>
+     */
+    public function getAllergies(): Collection
+    {
+        return $this->allergies;
+    }
+
+    public function addAllergy(Allergie $allergy): self
+    {
+        if (!$this->allergies->contains($allergy)) {
+            $this->allergies->add($allergy);
+            $allergy->addUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAllergy(Allergie $allergy): self
+    {
+        if ($this->allergies->removeElement($allergy)) {
+            $allergy->removeUser($this);
+        }
 
         return $this;
     }
