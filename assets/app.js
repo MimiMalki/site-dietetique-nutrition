3a808cbfcc8941676cb95cbf2fde05726044a612
Meryem Malki <meriamemalki96@gmail.com>
2023-05-30 10:58:30 +0200

    add home page

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,5 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-import './images/logo.png';
 // start the Stimulus application
-import './bootstrap';
+// import './bootstrap';
