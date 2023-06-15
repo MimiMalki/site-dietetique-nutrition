d6008c9f13abfc96b4c6cc6a083227f28794bbb2
Meryem Malki <meriamemalki96@gmail.com>
2023-06-15 17:59:01 +0200

    Update module contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ContacteController extends AbstractController
+class ContactController extends AbstractController
 {
-    #[Route('/contacte', name: 'app_contacte')]
+    #[Route('/contact', name: 'app_contact')]
     public function index(): Response
     {
-        return $this->render('contacte/index.html.twig', [
-            'controller_name' => 'ContacteController',
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
         ]);
     }
 }
